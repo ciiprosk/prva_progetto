3a5c73019e80efc66c39d76bdb738ced0a3f38d1
rosap <rosap@172.19.166.74>
2024-11-21 13:22:23 +0100

    primo commit rosa

---
+++ src/main/java/git/prova_git/Rosa.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git.prova_git;
+
+/**
+ *
+ * @author rosap
+ */
+public class Rosa {
+    private String Rosa="sei gay";
+}
