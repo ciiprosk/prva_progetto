8e3c6e328dee55b6d9a513c3463b75d7c56385f2
anton <anton@172.19.206.117>
2024-11-21 13:12:43 +0100

    Beccateve sto codice

---
+++ src/main/java/git/prova_git/AntonioDioooooooooo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git.prova_git;
+
+/**
+ *
+ * @author anton
+ */
+public class AntonioDioooooooooo {
+    public static void main(String[] args) {
+        //CIAO ROSA! STAI LEGGENDO?
+        System.out.println("Dare moneta,");
+        System.out.println("Salvo anima!");
+    }
+}
