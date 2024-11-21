ea013bd67ad95f3c8abb725bcbab626054bf97fd
ix5lo <ix5lo@172.19.206.98>
2024-11-21 13:01:34 +0100

    prova nuova classe vincenzo

---
+++ src/main/java/git/prova_git/Vincenzo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git.prova_git;
+
+/**
+ *
+ * @author ix5lo
+ */
+public class Vincenzo {
+    private final String prova="prova per il commit condiviso";
+}
